94bef1d9bb568a1b6f9b47248e92147ede883ee0
feiyin0719 <582816317@qq.com>
2025-11-29 23:29:55 +0800

    support hanlde custom event

--- markdown/src/main/java/com/iffly/compose/markdown/ActionHandler.kt
+++ markdown/src/main/java/com/iffly/compose/markdown/ActionHandler.kt
@@ -4,7 +4,26 @@ import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.LinkInteractionListener
 import com.vladsch.flexmark.util.ast.Node
 
+/**
+ * Marker interface for custom events.
+ * Implement this interface to define specific custom events that can be handled
+ * by the ActionHandler.
+ * When you create a custom node in markdown, you can also create a corresponding CustomEvent
+ * implementation to represent events related to that node.
+ */
+interface CustomEvent
+
+/**
+ * Interface for handling various user actions within markdown content.
+ * Implement this interface to define custom behavior for URL clicks, copy actions,
+ * image clicks, and other custom events.
+ */
 interface ActionHandler {
+    /**
+     * Handle URL click events.
+     * @param url The URL that was clicked.
+     * @param node The markdown node associated with the URL.
+     */
     fun handleUrlClick(
         url: String,
         node: Node,
@@ -12,12 +31,33 @@ interface ActionHandler {
         // Default implementation does nothing
     }
 
+    /**
+     * Handle copy click events.
+     * @param node The markdown node associated with the copy action.
+     */
     fun handleCopyClick(node: Node) {
         // Default implementation does nothing
     }
 
+    /**
+     * Handle image click events.
+     * @param imageUrl The URL of the image that was clicked.
+     * @param node The markdown node associated with the image.
+     */
     fun handleImageClick(
         imageUrl: String,
+        node: Node,
+    ) {
+        // Default implementation does nothing
+    }
+
+    /**
+     * Handle custom events.
+     * @param event The custom event to handle.
+     * @param node The markdown node associated with the event.
+     */
+    fun handleCustomEvent(
+        event: CustomEvent,
         node: Node,
     ) {
         // Default implementation does nothing
